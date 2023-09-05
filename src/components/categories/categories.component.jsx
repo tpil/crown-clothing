f8b7bff64d57d9cd7cd02836607600369c0e8ba1
teopyl <teopyl@gmail.com>
2023-09-05 13:34:52 +0300

    categories and category-item components, new font added

---
+++ src/components/categories/categories.component.jsx
@@ -0,0 +1,15 @@
+import './categories.styles.scss';
+import CategoryItem from '../category-item/category-item.component';
+
+const Category = ({categories}) => {
+    
+    return (
+        <div className="categories-container">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    );
+};
+
+export default Category;
