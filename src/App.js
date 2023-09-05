ffd5f35762195c49af3376753a7b2725c7e877f5
teopyl <teopyl@gmail.com>
2023-09-05 16:37:47 +0300

    react router added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import "./App.css";
-import Category from "./components/categories/categories.component";
+import Home from "./routes/home.component";
+import Navigation from "./routes/navigation-bar/navigation-bar.component";
+import { Routes, Route } from "react-router-dom";
+
+const Shop = () => {
+  return <div>Shop</div>;
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
   return (
-    <Category categories={categories}/>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/routes/navigation-bar/navigation-bar.component.jsx
@@ -0,0 +1,24 @@
+import { Link, Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import "./navigation-bar.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <div className="logo-container">
+          <Link className="nav-link" to="/">
+            <CrownLogo className="logo" />
+          </Link>
+        </div>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">SHOP</Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
