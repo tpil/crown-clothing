48729567b5fa3068d49dd16586dbd9cdcd45311d
teopyl <teopyl@gmail.com>
2023-09-07 20:28:57 +0300

    remove unnecessary imports

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,15 +1,4 @@
-import { useEffect, useState } from "react";
-import {
-  auth,
-  signInWithGoogleRedirect,
-  signInWithUserEmailAndPassword,
-  signInWithGooglePopup,
-  createUserDocFromAuth,
-} from "../../utils/firebase.utils";
-import { getRedirectResult } from "firebase/auth";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
-import Button from "../../components/button/button.components";
-import FormInput from "../../components/form-input/form-input.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 import './authentication.styles.scss';
 
